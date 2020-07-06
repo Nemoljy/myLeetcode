0736e60e55e8b3d83f269f3791677783f9e3c3ba
Nemoljy <32947114+Nemoljy@users.noreply.github.com>
2020-07-06 21:58:45 +0800

    Create 1Solution2.py

---
+++ src/Hash/1Solution2.py
@@ -0,0 +1,16 @@
+## 字典 O(n)
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        for index, num in enumerate(nums):
+            print(hashmap)
+            another_num = target - num
+            if another_num in hashmap:
+                return [hashmap[another_num], index]
+            hashmap[num] = index
+        return None
